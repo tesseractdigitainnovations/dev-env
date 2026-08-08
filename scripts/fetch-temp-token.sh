934d44e0d2c8a280346a07a0af184cbf56e900ff
crypt0g30rgy <crypt0g30rgy@users.noreply.github.com>
2026-08-08 14:47:03 +0300

    improvements

--- scripts/fetch-temp-token.sh
+++ scripts/fetch-temp-token.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Fetch credentials with a 15-second timeout to avoid getting stuck if AWS endpoints are unreachable
-if ! CREDS=$(timeout 15s aws sts get-session-token --duration-seconds 3600 --profile default 2>/dev/null); then
+if ! CREDS=$(timeout 60s aws sts get-session-token --duration-seconds 3600 --profile default 2>/dev/null); then
   echo "Error: Failed to fetch temporary AWS credentials or command timed out." >&2
   exit 1
 fi
@@ -18,4 +18,3 @@ jq '{
   SessionToken: .Credentials.SessionToken,
   Expiration: .Credentials.Expiration
 }' <<< "$CREDS"
-
